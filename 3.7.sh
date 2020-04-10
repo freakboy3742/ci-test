f7e2d8cfe0edbffcdac9901d865196cef3a17077
Russell Keith-Magee <russell@keith-magee.com>
2020-04-10 11:43:10 +0800

    Use zip.

--- 3.7.sh
+++ 3.7.sh
@@ -5,8 +5,8 @@ echo "BUILDING TARBALL FOR 3.7"
 mkdir -p dist
 if [ -z "${BUILD_NUMBER}" ]; then
     echo "No build number"
-    tar zcvf dist/Python-3.7-Android-support.tar.gz content
+    zip dist/Python-3.7-Android-support.tar.gz -r content
 else
     echo "Build number ${BUILD_NUMBER}"
-    tar zcvf dist/Python-3.7-Android-support.${BUILD_NUMBER}.tar.gz content
+    zip dist/Python-3.7-Android-support.${BUILD_NUMBER}.zip -r content
 fi
